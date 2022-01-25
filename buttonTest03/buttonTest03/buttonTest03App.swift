435db375dcfcff87b94afd277c3025b8b387c09d
Aika-n <92354035+Aika-n@users.noreply.github.com>
2022-01-25 11:52:29 +0900

    buttonTest03

---
+++ buttonTest03/buttonTest03/buttonTest03App.swift
@@ -0,0 +1,17 @@
+//
+//  buttonTest03App.swift
+//  buttonTest03
+//
+//  Created by it01 on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct buttonTest03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
